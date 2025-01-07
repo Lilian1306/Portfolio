dd5e71787395726a8d6ef819135cac9cd79aecbd
DaniM0613 <wwh.rj.92@gmail.com>
2025-01-06 23:46:59 -0600

    vercel y analytics

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import ScrollToTop from "./utils/ScrollToTop";
 
 const Navbar = lazy(() => import("./Pages/Navbar"));
@@ -8,10 +9,14 @@ const Servicios = lazy(() => import("./Pages/Servicios"));
 const Proyectos = lazy(() => import("./Pages/Proyectos"));
 const ContactUs = lazy(() => import("./Pages/ContactUs"));
 
+
+
 const Router = () => {
   return (
+    
     <BrowserRouter>
       <ScrollToTop />
+      <Analytics/>
       <Routes>
         <Route element={<Navbar />}>
           <Route path="/" element={<HeroSection />} />
